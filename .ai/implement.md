73ebdf411d7ed3c65c0a5847e97526eb7b8bb3f4
psmyrdek <przemek.smyrdek@gmail.com>
2025-03-23 11:33:57 +0100

    feat: init

---
+++ .ai/implement.md
@@ -0,0 +1,3 @@
+Zaimplementuj funkcjonalność banku w oparciu o poniższe wymagania: <SPEC>
+
+Gotowa implementacja powinna być zgodna z testami: <TEST>
---
+++ banking/banking.ts
@@ -0,0 +1,13 @@
+import {
+  BankAccount,
+  WithdrawalRequest,
+  WithdrawalResult,
+  WithdrawalError,
+} from "./types";
+
+export function createAccount(account: BankAccount) {}
+
+export function processWithdrawal(
+  account: BankAccount,
+  withdrawal: WithdrawalRequest
+): WithdrawalResult | WithdrawalError {}
